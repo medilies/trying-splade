fa60680e2566851e82c9e8c7e89122287f987d61
ilies <ilies@swissdidata.com>
2022-10-10 23:28:51 +0100

    readme

--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,25 @@ use ElaborateCode\AlgerianProvinces\Models\Wilaya;
 use Illuminate\Support\Facades\Route;
 use ProtoneMedia\Splade\SpladeTable;
 
+
+Route::get('/', function () {
+
+    $wilayas = Wilaya::paginate(10);
+
+    return view('home')
+        ->with(
+            'wilayas',
+            SpladeTable::for($wilayas)
+                ->column('id')
+                ->column('fr_name')
+                ->column('ar_name')
+        );
+})
+    ->middleware(['splade'])
+    ->name('home');
+
+
 Route::middleware(['splade'])
     ->group(function () {
-        Route::get('/', function () {
-
-            $wilayas = Wilaya::paginate(10);
-
-            return view('home')
-                ->with(
-                    'wilayas',
-                    SpladeTable::for($wilayas)
-                        ->column('id')
-                        ->column('fr_name')
-                        ->column('ar_name')
-                );
-        })->name('home');
         Route::get('/docs', fn () => view('docs'))->name('docs');
     });
