9eb321400d11bc5b1a08614e27170c785c8fa1fe
ilies <ilies@swissdidata.com>
2022-10-10 23:15:30 +0100

    Added elaborate-code/laravel-algerian-provinces

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,23 @@
 <?php
 
+use ElaborateCode\AlgerianProvinces\Models\Wilaya;
 use Illuminate\Support\Facades\Route;
+use ProtoneMedia\Splade\SpladeTable;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::middleware(['splade'])
+    ->group(function () {
+        Route::get('/', function () {
 
-Route::middleware(['splade'])->group(function () {
-    Route::get('/', fn () => view('home'))->name('home');
-    Route::get('/docs', fn () => view('docs'))->name('docs');
-});
+            $wilayas = Wilaya::paginate(10);
+
+            return view('home')
+                ->with(
+                    'wilayas',
+                    SpladeTable::for($wilayas)
+                        ->column('id')
+                        ->column('fr_name')
+                        ->column('ar_name')
+                );
+        })->name('home');
+        Route::get('/docs', fn () => view('docs'))->name('docs');
+    });
