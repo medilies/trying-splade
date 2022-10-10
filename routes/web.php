b821eafb4d1da488deb24f8f859b8f32e12757d9
ilies <ilies@swissdidata.com>
2022-10-10 23:04:20 +0100

    Splade Automatic Installation

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['splade'])->group(function () {
+    Route::get('/', fn () => view('home'))->name('home');
+    Route::get('/docs', fn () => view('docs'))->name('docs');
 });
